2dfa46945d3147fb07e6e107272ef4e1f4cfa658
Zwe Yan Naing <ericthedev97@gmail.com>
2023-02-15 14:58:32 +0630

    admin dashboard set up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/login',[PageController::class,'showLogin']);
+Route::prefix('admin')->group(function (){
+   Route::get('/',[PageController::class,'showDashboard']);
+});
